3ade81ab9c0f537346ec9ffdf474b62946bb71e1
Nathan Wold <natew79@gmail.com>
2019-09-06 17:33:52 -0500

    local host server up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,24 @@
 console.log('JQ');
 
 $(document).ready(function() {
-
+    $('#equals').on('click', equateButton);
+    $('#clear').on('click', clearButton);
 });
+
+function grabOperator(){
+    let operator = $(this).attr('id');
+    return operator;
+}
+
+function equateButton(){
+    let equation = {
+        firstnumber: $('#firstNumber').val(),
+        secondnumber: $('#secondNumber').val(),
+        operator: grabOperator()
+    }
+    console.log(equation);
+}
+
+function clearButton(){
+
+}
